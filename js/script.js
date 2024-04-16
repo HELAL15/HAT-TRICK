527ed5c88ff1df6655188ecde3399f10da59ed17
HELAL15 <ah4459577@gmail.com>
2024-04-16 15:49:02 +0200

    final without animations

--- js/script.js
+++ js/script.js
@@ -68,40 +68,56 @@ if (play !== null) {
 
     
 
-// function showBG() {
-//   let scroll = window.scrollY;
-//   let isNavTop = scroll < 50;
-//   let nav = document.getElementById('nav');
-//   if ( isNavTop) {
-//     nav.classList.remove('scrolling');
-//   } else {
-//     nav.classList.add('scrolling');
-//   }
-// }
-// let nav = document.getElementById('nav');
-// if(nav != null){
-//   window.addEventListener('scroll', showBG);
-// }
+function showScrollTopBtn() {
+  let scroll = window.scrollY;
+  let isNavTop = scroll < 250;
+  let scrollBtn = document.querySelector('.scrollTop');
+  if ( isNavTop) {
+    scrollBtn.classList.remove('show');
+    
+  } else {
+    scrollBtn.classList.add('show');
+  }
+}
+let scrollBtn = document.querySelector('.scrollTop');
+
+if(scrollBtn != null){
+  window.addEventListener('scroll', showScrollTopBtn);
+  scrollBtn.addEventListener("click", ()=>{
+    window.scrollTo({top:0, left:0 });
+  })
+}
 
 
-    // const navToggler = document.getElementById("nav-toggler");
-    // const collapse = document.querySelector(".navbar-collapse");
-    // const close = document.getElementById("close");
+    const navToggler = document.getElementById("nav-toggler");
+    const collapse = document.querySelector(".navbar-collapse");
+    const close = document.getElementById("close");
 
-    // close.addEventListener("click" , ()=>{
-    //   collapse.classList.remove("active")
-    // })
+    close.addEventListener("click" , ()=>{
+      collapse.classList.remove("active")
+    })
 
-    // navToggler.addEventListener("click" , ()=>{
-    //   collapse.classList.toggle("active")
-    // })
-
-
+    navToggler.addEventListener("click" , ()=>{
+      collapse.classList.toggle("active")
+    })
 
 
-    const headerSwiper = new Swiper('header .swiper', {
-      slidesPerView: 1,
-      
+
+
+    const screensSwiper = new Swiper('.screens .swiper', {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      breakpoints: {
+        320: {
+          slidesPerView: 1.5,
+        },
+        640: {
+          slidesPerView: 2.5,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
       grid: {
         rows: 1,
       },
@@ -115,7 +131,7 @@ if (play !== null) {
 
       // Autoplay settings
       autoplay: {
-        delay: 8000, 
+        delay: 3000, 
         disableOnInteraction: false, 
       },
 
@@ -131,9 +147,7 @@ if (play !== null) {
 
 
 
-
     //end
-    
 });
 
 
